99e654c89129064146856fbc30b773143fe8747a
Jun Park <junpark@Juns-MacBook-Pro.local>
2024-10-08 11:53:53 -0400

    Organizing Everything

--- hw4/partA.m
+++
@@ -1,4 +0,0 @@
-% Apply low pass filtering by multiplication in frequency domain. Show filtered image in spatial domain.
-% Apply high pass filtering by multiplication in frequency domain. Show filtered image in spatial domain.
-% Add noise to your images. Does spatial domain filtering (median filtering, spatial domain gauss filter, etc.) work better to remove this noise or frequency domain filtering? 
-
